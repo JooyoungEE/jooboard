68ab612216d2dc3722bcb3e9af2328d3e1f5e67a
hsclever <hsjsclever@daum.net>
2021-02-22 11:37:38 +0900

    충돌 수정

--- src/main/java/com/jooboard/joo/controller/BoardController.java
+++ src/main/java/com/jooboard/joo/controller/BoardController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.jooboard.joo.controller;
 
 import java.util.*;
@@ -88,80 +87,8 @@ public class BoardController {
 		return mv;
 	}
 }
-=======
-package com.jooboard.joo.controller;
 
-import java.util.*;
 
-import org.springframework.beans.factory.annotation.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.jooboard.joo.dao.BoardDao;
-import com.jooboard.joo.service.BoardService;
-import com.jooboard.joo.util.PageUtil;
-import com.jooboard.joo.vo.BoardVO;
 
-@Controller
-@RequestMapping("/board")
-public class BoardController {
-	@Autowired
-	BoardDao bDao;
 
-	@Autowired
-	BoardService bSrvc;
-	
-	//게시판 리스트
-	@RequestMapping("/boardList.joo") 
-	public ModelAndView board(ModelAndView mv, PageUtil page) {
-		int total = bDao.getTotal();
-		page.setPage(total, 5, 5);
-		
-		List<BoardVO> list = bDao.getList(page);
-		
-		mv.addObject("PAGE", page);
-		mv.addObject("LIST", list);
-		
-		mv.setViewName("board/boardList");
-		return mv;
-	}
-	
-	//게시글 상세보기
-	@RequestMapping(value="/boardDetail.joo", method= {RequestMethod.POST, RequestMethod.GET})
-	public ModelAndView boardDetail(ModelAndView mv, int bno) {
-		BoardVO bVO = bDao.getDetail(bno);
-		mv.addObject("DATA", bVO);
-		
-		mv.setViewName("board/boardDetail");
-		return mv;
-	}
-
-	//게시글 수정 폼보기
-	@RequestMapping(value="/boardEdit.joo", method= {RequestMethod.POST})
-	public ModelAndView boardEdit(ModelAndView mv, BoardVO bVO, PageUtil page) {
-		int bno = bVO.getBno();
-		System.out.println("test1 "+bVO.getBno());
-		List<Map> list = bDao.getEditDetail(bno);
-
-		mv.addObject("LIST", list);
-		mv.addObject("PAGE", page);
-		mv.setViewName("board/boardEdit");
-		return mv;
-	}
-	
-	//게시글 수정
-	@RequestMapping("/boardEditProc.joo")
-	public ModelAndView boardEditProc(BoardVO bVO, ModelAndView mv, PageUtil page) {
-		bSrvc.editBoard(mv, bVO, page);
-		return mv;
-	}
-	
-	//게시글 삭제
-	@RequestMapping("/boardDel.joo")
-	public ModelAndView boardDel(ModelAndView mv, BoardVO bVO, PageUtil page) {
-		bSrvc.delBoard(mv, bVO, page);
-		return mv;
-	}
-}
->>>>>>> cd8214775e8e9234198a478c8a270442d919eab1
--- src/main/java/com/jooboard/joo/dao/BoardDao.java
+++ src/main/java/com/jooboard/joo/dao/BoardDao.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.jooboard.joo.dao;
 
 import java.util.*;
@@ -50,51 +50,4 @@ public class BoardDao {
 		return sqlSession.insert("mSQL.writeBoard", bVO);
 	}
 }
-=======
-package com.jooboard.joo.dao;
 
-import java.util.*;
-
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.*;
-import org.springframework.stereotype.Repository;
-
-import com.jooboard.joo.util.PageUtil;
-import com.jooboard.joo.vo.BoardVO;
-
-@Repository
-public class BoardDao {
-	@Autowired
-	SqlSessionTemplate sqlSession;
-	
-	//게시글 갯수 조회
-	public int getTotal() {
-		return sqlSession.selectOne("mSQL.getTotal");
-	}
-	
-	//게시글 리스트 조회
-	public List<BoardVO> getList(PageUtil page){
-		return sqlSession.selectList("mSQL.getList", page);
-	}
-	
-	//게시글 내용 조회
-	public BoardVO getDetail(int bno) {
-		return sqlSession.selectOne("mSQL.getDetail", bno);
-	}
-	
-	// 게시글 수정 내용조회
-	public List<Map> getEditDetail(int bno){
-		return sqlSession.selectList("mSQL.getEditDetail", bno);
-	}
-	
-	// 게시글 수정
-	public int editBoard(BoardVO bVO) {
-		return sqlSession.update("mSQL.editBoard", bVO);
-	}
-	
-	//게시글 삭제
-	public int delBoard(int bno) {
-		return sqlSession.update("mSQL.delBoard", bno);
-	}
-}
->>>>>>> cd8214775e8e9234198a478c8a270442d919eab1
